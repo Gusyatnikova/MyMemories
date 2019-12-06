2f6a9f95bb3f57520b422684641809d2cabace65
Gusyatnikova <crysnat08@gmail.com>
2019-12-06 20:55:40 +0700

    Add User.java full realization

--- MyMemories/src/main/java/com/example/mymemories/model/User.java
+++ MyMemories/src/main/java/com/example/mymemories/model/User.java
@@ -7,23 +7,30 @@ public class User {
     private ArrayList<Note> notes;
 
     public User(){
+        login = null;
     }
 
     public User(String Login){
+        login = Login;
     }
 
     public String getLogin() {
+        return login;
     }
 
     public ArrayList<Note> getNotes() {
+        return notes;
     }
 
     public void setLogin(String login) {
+        this.login = login;
     }
 
     public void setNotes(Note note) {
+        this.notes.add(note);
     }
 
     public void setNotes(ArrayList<Note> notes) {
+        this.notes = notes;
     }
 }
