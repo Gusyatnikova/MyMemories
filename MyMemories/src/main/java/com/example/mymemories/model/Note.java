c536355b7fccf0c3c69b089e24b476391d3f40c1
Gusyatnikova <crysnat08@gmail.com>
2019-12-06 20:49:39 +0700

    Add contract of class Note.java
    Need to be realized

---
+++ MyMemories/src/main/java/com/example/mymemories/model/Note.java
@@ -0,0 +1,43 @@
+package com.example.mymemories.model;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Note {
+    private String title;
+    private String content;
+    private String date;
+    private ArrayList<File> resources;
+
+    public Note(String Title, String Content, String Date, String Resources){
+    }
+
+    //TODO парсинг строки ресурсов
+    private void parseResources(String res){
+
+    }
+
+    public String getTitle() {
+    }
+
+    public String getContent() {
+    }
+
+    public String getDate() {
+    }
+
+    public ArrayList<File> getResources() {
+    }
+
+    public void setTitle(String title) {
+    }
+
+    public void setContent(String content) {
+    }
+
+    public void setDate(String date) {
+    }
+
+    public void setResources(ArrayList<File> resources) {
+    }
+}
