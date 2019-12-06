0291593b05a532dee3feb4f185891bc6a6d1588d
Gusyatnikova <crysnat08@gmail.com>
2019-12-06 20:50:54 +0700

    Add full Note.java class

--- MyMemories/src/main/java/com/example/mymemories/model/Note.java
+++ MyMemories/src/main/java/com/example/mymemories/model/Note.java
@@ -10,6 +10,10 @@ public class Note {
     private ArrayList<File> resources;
 
     public Note(String Title, String Content, String Date, String Resources){
+        title = Title;
+        content = Content;
+        date = Date;
+        parseResources(Resources);
     }
 
     //TODO парсинг строки ресурсов
@@ -18,26 +22,34 @@ public class Note {
     }
 
     public String getTitle() {
+        return title;
     }
 
     public String getContent() {
+        return content;
     }
 
     public String getDate() {
+        return date;
     }
 
     public ArrayList<File> getResources() {
+        return resources;
     }
 
     public void setTitle(String title) {
+        this.title = title;
     }
 
     public void setContent(String content) {
+        this.content = content;
     }
 
     public void setDate(String date) {
+        this.date = date;
     }
 
     public void setResources(ArrayList<File> resources) {
+        this.resources = resources;
     }
 }
