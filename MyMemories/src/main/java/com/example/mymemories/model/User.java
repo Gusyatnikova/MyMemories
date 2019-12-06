a055ca5d0266585bd8c33d2fd639d2c6cc30ba01
Gusyatnikova <crysnat08@gmail.com>
2019-12-06 20:53:17 +0700

    Add User.java without realization
    Need to be finished

---
+++ MyMemories/src/main/java/com/example/mymemories/model/User.java
@@ -0,0 +1,29 @@
+package com.example.mymemories.model;
+
+import java.util.ArrayList;
+
+public class User {
+    private String login;
+    private ArrayList<Note> notes;
+
+    public User(){
+    }
+
+    public User(String Login){
+    }
+
+    public String getLogin() {
+    }
+
+    public ArrayList<Note> getNotes() {
+    }
+
+    public void setLogin(String login) {
+    }
+
+    public void setNotes(Note note) {
+    }
+
+    public void setNotes(ArrayList<Note> notes) {
+    }
+}
