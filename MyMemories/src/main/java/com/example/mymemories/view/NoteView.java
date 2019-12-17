99303b2435d9cab40ae31609a3eda006b2bd9e9c
Anna Miryanova <32086686+AnnMir@users.noreply.github.com>
2019-12-17 13:46:57 +0700

    Connect xml with NoteView

--- MyMemories/src/main/java/com/example/mymemories/view/NoteView.java
+++ MyMemories/src/main/java/com/example/mymemories/view/NoteView.java
@@ -1,16 +1,33 @@
 package com.example.mymemories.view;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
-import android.os.Bundle;
+        import com.example.mymemories.R;
+        import com.example.mymemories.model.User;
 
-import com.example.mymemories.R;
+        import androidx.appcompat.app.AppCompatActivity;
 
 public class NoteView extends AppCompatActivity {
+    TextView title;
+    TextView date;
+    EditText content;
+    String uuid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_view);
+        title = findViewById(R.id.title);
+        date = findViewById(R.id.date);
+        content = findViewById(R.id.content);
+        initValues();
+    }
+
+    public void initValues(){
+        //TODO
     }
 }
