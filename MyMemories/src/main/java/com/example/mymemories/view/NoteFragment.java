105d97730b44e3379f9b053c3dd49f918289e233
Анна Мирянова <a.mirianova@g.nsu.ru>
2019-12-17 12:22:37 +0700

    Add SimpleAdapter

--- MyMemories/src/main/java/com/example/mymemories/view/NoteFragment.java
+++ MyMemories/src/main/java/com/example/mymemories/view/NoteFragment.java
@@ -1,51 +1,29 @@
 package com.example.mymemories.view;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-import com.example.mymemories.model.ShortNote;
-import com.example.mymemories.model.User;
-
+import android.widget.SimpleAdapter;
+import com.example.mymemories.R;
 import java.util.ArrayList;
-import java.util.List;
-
+import java.util.HashMap;
 import androidx.fragment.app.ListFragment;
 
 public class NoteFragment extends ListFragment {
-    private static List<ShortNote> notes = new ArrayList<ShortNote>();
+    private ArrayList<HashMap<String, String>> notes = new ArrayList<>();
+    private HashMap<String, String> map;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        notes = User.getUser().getShortNotes();
-        ArrayAdapter<ShortNote> adapter = new NoteAdapter(this.getContext());
+        setNotes();
+        SimpleAdapter adapter = new SimpleAdapter(this.getContext(), notes,
+                R.layout.note_fragment,
+                new String[]{"Title", "Date", "UUID"},
+                new int[]{R.id.title, R.id.date,
+                        R.id.uuid});
         setListAdapter(adapter);
     }
 
-    private class NoteAdapter extends ArrayAdapter<ShortNote> {
-
-        public NoteAdapter(Context context) {
-            super(context, android.R.layout.simple_list_item_2, notes);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ShortNote shortNote = getItem(position);
-
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext())
-                        .inflate(android.R.layout.simple_list_item_2, null);
-            }
-            ((TextView) convertView.findViewById(android.R.id.text1))
-                    .setText(shortNote.title);
-            ((TextView) convertView.findViewById(android.R.id.text2))
-                    .setText(shortNote.date);
-            return convertView;
-        }
+    private void setNotes(){
+        //TODO
     }
 }
